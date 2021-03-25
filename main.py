bc4e63568a536fe2111bad949f1002f9d63c6199
Paul ROUSSEAU <paul.rousseau39@gmail.com>
2021-03-25 11:04:22 +0000

    menu() terminée et commenté

--- main.py
+++ main.py
@@ -7,67 +7,6 @@ hideturtle()
 #On met la vitesse maximale pour la tortue
 speed(0)
 
-#Fonction qui génère et dessine un flocon de koch
-def flocon_de_koch (nombre_iteration, couleur):
-  #On met un assert pour que le nombre d'itération soit bien un nombre entier et que la boucle puisse se faire. De plus, il faut que ce nombre soit positif ou nul mais on le limite a 4 maximum pour que ça ne prenne pas trop de temps et que ça reste avec une épaisseur et un centrage acceptable.
-  assert type(nombre_iteration) == int and nombre_iteration >= 0 and nombre_iteration <= 4
-  #On définit les variables initiales.
-  #Variable qui stocke de combien est-ce que la tortue avance lorsqu'elle a une instruction qui lui dit d'avancer
-  avant = 200
-  #Variable qui stocke l'épaisseur de la tortue
-  epaisseur = 10
-  #Variable qui stocke le décalage de la tortue au début
-  decalage = 100
-  #Variable qui stock de combien de degrés la tortue doit tourner lorsqu'elle a une instruction qui lui dit de tourner
-  angle = 60
-  #Variable qui stock la chaine de départ
-  chaine_actuelle = "F++F++F"
-  #Variable qui stock les regles de création de la chaine
-  regle = ["F", "F-F++F-F"]
-  #Génère la chaine
-  #Fait une boucle qui tourne autant de fois qu'on veut itérer la chaine
-  for i in range(nombre_iteration):
-    #A chaque itération la chaine et donc le dessin devient de plus en plus gros donc on rapetisse les variables d'avancement et d'épaisseur pour que la taille reste convenable
-    avant = avant * 0.37
-    epaisseur = epaisseur * 0.6
-    #A chaque itération, on décale le dessin a chaque fois un peu plus du centre pour qu'il reste centré
-    decalage = decalage * 1.02
-    #On créé une nouvelle chaine vide
-    nouvelle_chaine = ""
-    #On parcours tous les caractères de l'ancienne chaine
-    for caractere in chaine_actuelle:
-      #Si on rencontre le caractère de la regle alors on ajoute à la nouvelle chaine les caractères correspondants
-      if caractere == regle[0]:
-        nouvelle_chaine = nouvelle_chaine + regle[1]
-      #Si on ne trouve pas de caractères qui correpond a la regle alors on remet le même caractère
-      else:
-        nouvelle_chaine = nouvelle_chaine + caractere
-    #On remplace l'ancienne chaine par la nouvelle
-    chaine_actuelle = nouvelle_chaine
-  #Initialisation de la tortue
-  #On se déplace à l'endroit où l'on commence à dessiner
-  up()
-  goto(-decalage, decalage)
-  #On fixe la direction sur 0 pour être que ce soit la direction souhaitée
-  setheading(0)
-  down()
-  #On met l'épaisseur a la valeur définie auparavant
-  width(epaisseur)
-  #On définie la couleur
-  color(couleur)
-  #Dessine
-  #Parcours tous les caractères de la chaine
-  for caractere in chaine_actuelle:
-    #Si l'on rencontre un F alors on avance de la valeur définie auparavant
-    if caractere == "F":
-      forward(avant)
-    #Si l'on rencontre un + alors on tourne a droite de la valeur définie auparavant
-    elif caractere == "+":
-      right(angle)
-    #Si l'on rencontre un - alors on tourne a gauche de la valeur définie auparavant
-    elif caractere == "-":
-      left(angle)
-
 #Fonction qui génère et dessine une courbe de koch
 def courbe_de_koch (nombre_iteration, couleur):
   #On met un assert pour que le nombre d'itération soit bien un nombre entier et que la boucle puisse se faire. De plus, il faut que ce nombre soit positif ou nul mais on le limite a 5 maximum pour que ça ne prenne pas trop de temps et que ça reste avec une épaisseur et un centrage acceptable.
@@ -111,6 +50,67 @@ def courbe_de_koch (nombre_iteration, couleur):
   goto(decalage, -decalage)
   #On fixe la direction à laquelle on veut que la tortue commence
   setheading(180)
+  down()
+  #On met l'épaisseur a la valeur définie auparavant
+  width(epaisseur)
+  #On définie la couleur
+  color(couleur)
+  #Dessine
+  #Parcours tous les caractères de la chaine
+  for caractere in chaine_actuelle:
+    #Si l'on rencontre un F alors on avance de la valeur définie auparavant
+    if caractere == "F":
+      forward(avant)
+    #Si l'on rencontre un + alors on tourne a droite de la valeur définie auparavant
+    elif caractere == "+":
+      right(angle)
+    #Si l'on rencontre un - alors on tourne a gauche de la valeur définie auparavant
+    elif caractere == "-":
+      left(angle)
+
+#Fonction qui génère et dessine un flocon de koch
+def flocon_de_koch (nombre_iteration, couleur):
+  #On met un assert pour que le nombre d'itération soit bien un nombre entier et que la boucle puisse se faire. De plus, il faut que ce nombre soit positif ou nul mais on le limite a 4 maximum pour que ça ne prenne pas trop de temps et que ça reste avec une épaisseur et un centrage acceptable.
+  assert type(nombre_iteration) == int and nombre_iteration >= 0 and nombre_iteration <= 4
+  #On définit les variables initiales.
+  #Variable qui stocke de combien est-ce que la tortue avance lorsqu'elle a une instruction qui lui dit d'avancer
+  avant = 200
+  #Variable qui stocke l'épaisseur de la tortue
+  epaisseur = 10
+  #Variable qui stocke le décalage de la tortue au début
+  decalage = 100
+  #Variable qui stock de combien de degrés la tortue doit tourner lorsqu'elle a une instruction qui lui dit de tourner
+  angle = 60
+  #Variable qui stock la chaine de départ
+  chaine_actuelle = "F++F++F"
+  #Variable qui stock les regles de création de la chaine
+  regle = ["F", "F-F++F-F"]
+  #Génère la chaine
+  #Fait une boucle qui tourne autant de fois qu'on veut itérer la chaine
+  for i in range(nombre_iteration):
+    #A chaque itération la chaine et donc le dessin devient de plus en plus gros donc on rapetisse les variables d'avancement et d'épaisseur pour que la taille reste convenable
+    avant = avant * 0.37
+    epaisseur = epaisseur * 0.6
+    #A chaque itération, on décale le dessin a chaque fois un peu plus du centre pour qu'il reste centré
+    decalage = decalage * 1.02
+    #On créé une nouvelle chaine vide
+    nouvelle_chaine = ""
+    #On parcours tous les caractères de l'ancienne chaine
+    for caractere in chaine_actuelle:
+      #Si on rencontre le caractère de la regle alors on ajoute à la nouvelle chaine les caractères correspondants
+      if caractere == regle[0]:
+        nouvelle_chaine = nouvelle_chaine + regle[1]
+      #Si on ne trouve pas de caractères qui correpond a la regle alors on remet le même caractère
+      else:
+        nouvelle_chaine = nouvelle_chaine + caractere
+    #On remplace l'ancienne chaine par la nouvelle
+    chaine_actuelle = nouvelle_chaine
+  #Initialisation de la tortue
+  #On se déplace à l'endroit où l'on commence à dessiner
+  up()
+  goto(-decalage, decalage)
+  #On fixe la direction sur 0 pour être que ce soit la direction souhaitée
+  setheading(0)
   down()
   #On met l'épaisseur a la valeur définie auparavant
   width(epaisseur)
@@ -354,38 +354,11 @@ def arbre_2D_aleatoire (nombre_iteration):
       #On fait l'opération inverse sur le facteur pour qu'il retrouve sa valeur d'avant
       facteur = facteur / 0.9      
 
-def menu_principal ():
-  print("Menu principal")
-  print("Veuillez choisir un motif à dessiner\n")
-  print("Courbe de Koch  (1)")
-  print("Flocon de Koch  (2)")
-  print("Arbre standard  (3)")
-  print("Arbre recursif  (4)")
-  print("Arbre aleatoire (5)")
-
+#Fonction qui met beaucoup de sauts a la ligne pour nettoyer la console lors de la navigation dans le menu
 def menu_espace ():
   print("\n\n\n\n\n\n\n\n\n\n\n")
 
-#choix [1:Courbe de Koch" , 2:"Flocon de Koch" , 3:"Arbre standard" , 4:"Arbre recursif" , 5:"Arbre aleatoire"]
-def menu_iteration (motif):
-  if motif == 1:
-    motif = "Courbe de Koch"
-  elif motif == 2:
-    motif = "Flocon de Koch"
-  elif motif == 3:
-    motif = "Arbre standard"
-  elif motif == 4:
-    motif = "Arbre recursif"
-  elif motif == 5:
-    motif = "Arbre aleatoire"
-  print("Vous avez choisi le motif " + motif)
-
-def menu_couleur ():
-  print("Veuillez choisir une couleur pour le motif")
-  print("Bleu  (1)")
-  print("Rouge (2)")
-  print("Vert  (3)")
-
+#Variable qui stocke ce que le menu doit proposer en fonction du choix du motif de l'utilisateur
 configuration_menu = {
   "1" : {
     "nom" : "Courbe de Koch",
@@ -413,6 +386,7 @@ configuration_menu = {
     "couleur" : False
   }
 }
+#Fonction qui stock les couleurs que l'utilisateur peut choisir et leur valeur hexadecimale a passer en argument des fonctions
 couleurs = {
   "1" : {
     "nom" : "Bleu",
@@ -440,137 +414,113 @@ couleurs = {
   }
 }
 
-def menu_test():
+#Fonction qui affiche le menu
+def menu():
+  #Affiche "Menu principal"
   print("Menu principal")
+  #Compte à partir de 1 pour indiquer la ligne à selectionner pour l'utilisateur
   numerotation = 1
+  #On fait une boucle pour tous les motifs disponibles dans le dictionnaire configuration_menu
   for numero_menu in configuration_menu:
+    #Et on affiche le nom et le numéro de la ligne en face
     print(configuration_menu[numero_menu]["nom"] + " (" + str(numerotation) + ")")
+    #A chaque ligne, on ajoute 1 a la numerotation de la ligne
     numerotation = numerotation + 1
+  #On demande le numero de la ligne a l'utilisateur et on stock sa reponse dans une variable locale
   reponse_motif = input("Veuillez entrer le nombre correspondant au motif a dessiner")
+  #Si la reponse n'est pas dans les choix possibles, alors on affiche une erreur et on revient au debut du menu
   if int(reponse_motif) < 1 or int(reponse_motif) > 5:
     menu_espace()
     print("Erreur, mauvais input. Retour au menu principal.\n")
-    menu_test()
+    menu()
+  #Si on peut choisir un nombre d'iteration pour ce motif... (pas pour l'arbre recursif pas exemple) (information recuperee dans configuration_menu)
   if configuration_menu[reponse_motif]["iteration_max"] > 0:
+    #On nettoie la console
     menu_espace()
+    #On affiche le motif que l'utilisateur a choisit pour qu'il voit si jamais il s'est trompe
     print("Vous avez choisit le motif " + configuration_menu[reponse_motif]["nom"])
+    #On demande le nombre d'iteration a l'utilisateur et on stock sa reponse dans une variable locale
     reponse_iteration = input("Veuillez indiquer le nombre d'iteration voulu (entre 0 et " + str(configuration_menu[reponse_motif]["iteration_max"]) + ")")
+    #Si la reponse n'est pas dans les choix possibles, alors on affiche une erreur et on revient au debut du menu
     if int(reponse_iteration) < 0 or int(reponse_iteration) > configuration_menu[reponse_motif]["iteration_max"]:
       menu_espace()
       print("Erreur, mauvais input. Retour au menu principal.\n")
-      menu_test()
+      menu()
+  #Si on peut choisir une couleur pour le motif...
   if configuration_menu[reponse_motif]["couleur"] == True:
+    #On nettoie la console
     menu_espace()
+    #On affiche le nombre d'iterations que l'utilisateur a choisit pour qu'il voit si jamais il s'est trompe
     print("Vous avez selectionne " + reponse_iteration + " iterations")
     print("Veuillez choisir une couleur pour le motif")
+    #Compte à partir de 1 pour indiquer la ligne à selectionner pour l'utilisateur
     numerotation = 1
+    #Pour chaque couleur dans la liste couleurs...
     for numero_couleur in couleurs:
+      #On affiche le nom de la couleur et le numero de la ligne
       print(couleurs[numero_couleur]["nom"] + "(" + str(numerotation) + ")")
+      #A chaque ligne, on ajoute 1 a la numerotation de la ligne
       numerotation = numerotation + 1
+    #On demande la couleur a l'utilisateur et on stock sa reponse dans une variable locale
     reponse_couleur = input("Veuillez entrer le nombre correspondant a la couleur choisit")
+    #Si la reponse n'est pas dans les choix possibles, alors on affiche une erreur et on revient au debut du menu
     if int(reponse_couleur) < 1 or int(reponse_couleur) > len(couleurs):
       menu_espace()
       print("Erreur, mauvais input. Retour au menu principal.\n")
-      menu_test()
+      menu()
+  #Une fois que tous les choix on ete faits...
+  #On nettoie la console
   menu_espace()
+  #On affiche le motif qui est en train de se dessiner
   print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est en train de se dessiner...")
+  #Si l'utilisateur avait selectionne la courbe de koch...
   if reponse_motif == "1":
+    #On la dessine avec les arguments choisit
     courbe_de_koch(int(reponse_iteration), couleurs[reponse_couleur]["Hexa"])
+    #Une fois que le motif est termine...
+    #On nettoie la console
     menu_espace()
+    #Et on affiche que le motif est termine avec son nom et  les parametres choisit si jamais l'utilisateur veut le refaire par la suite
     print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est termine ! (" + reponse_iteration + " iterations, couleur : " + couleurs[reponse_couleur]["nom"] + ")")
+  #Si l'utilisateur avait selectionne le flocon de koch...
   if reponse_motif == "2":
+    #On le dessine avec les arguments choisit
     flocon_de_koch(int(reponse_iteration), couleurs[reponse_couleur]["Hexa"])
+    #Une fois que le motif est termine...
+    #On nettoie la console
     menu_espace()
+    #Et on affiche que le motif est termine avec son nom et les parametres choisit si jamais l'utilisateur veut le refaire par la suite
     print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est termine ! (" + reponse_iteration + " iterations, couleur : " + couleurs[reponse_couleur]["nom"] + ")")
+  #Si l'utilisateur avait selectionne l'arbre standard...
   if reponse_motif == "3":
+    #On le dessine avec l'argument choisit
     arbre_2D(int(reponse_iteration))
+    #Une fois que le motif est termine...
+    #On nettoie la console
     menu_espace()
+    #Et on affiche que le motif est termine avec son nom et le parametre choisit si jamais l'utilisateur veut le refaire par la suite
     print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est termine ! (" + reponse_iteration + " iterations)")
+  #Si l'utilisateur avait selectionne l'arbre recursif...
   if reponse_motif == "4":
+    #On le dessine
     arbre_2D_recursif(90)
+    #Une fois que le motif est termine...
+    #On nettoie la console
     menu_espace()
+    #Et on affiche que le motif est termine avec son nom si jamais l'utilisateur veut le refaire par la suite
     print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est termine !")
+  #Si l'utilisateur avait selectionne l'arbre aleatoire...
   if reponse_motif == "5":
+    #On le dessine avec l'argument choisit
     arbre_2D_aleatoire(int(reponse_iteration))
+    #Une fois que le motif est termine...
+    #On nettoie la console
     menu_espace()
+    #Et on affiche que le motif est termine avec son nom et le parametre choisit si jamais l'utilisateur veut le refaire par la suite
     print("Le motif " + configuration_menu[reponse_motif]["nom"] + " est termine ! (" + reponse_iteration + " iterations)")
-    
 
+#Au debut du programme...
+#On nettoie la console pour que le menu s'affiche ensuite en partant du bas de l'ecran et ainsi etre regulier puisque si l'utilisateur fait une erreur, le menu sera affiche en partant du bas de l'ecran donc comme ça on est regulier.
 menu_espace()
-menu_test()
-
-def menu():
-  etat = "menu principal"
-  if etat == "menu principal":
-    menu_principal()
-    reponse_menu = int(input("Veuillez entrer le nombre correspondant au motif a dessiner"))
-    if reponse_menu < 1 or reponse_menu > 5:
-      print("Erreur, mauvais input")
-    elif reponse_menu == 1:
-      menu_espace()
-      menu_iteration(1)
-      reponse_iteration = int(input("Veuillez indiquer le nombre d'iteration voulu (entre 0 et 5)"))
-      if reponse_iteration < 0 or reponse_iteration > 5:
-        print("Erreur, mauvais input")
-      else:
-        menu_espace()
-        menu_couleur()
-        reponse_couleur = int(input("Veuillez entrer le nombre correspondant a la couleur choisit"))
-        menu_espace()
-        if reponse_couleur < 1 or reponse_couleur > 3:
-          print("Erreur, mauvais input")
-        elif reponse_couleur == 1:
-          courbe_de_koch(reponse_iteration, "#5766b9")
-        elif reponse_couleur == 2:
-          courbe_de_koch(reponse_iteration,  "#b97c57")
-        elif reponse_couleur == 3:
-          courbe_de_koch(reponse_iteration, " #57b9a1 ")
-    elif reponse_menu == 2:
-      menu_espace()
-      menu_iteration(2)
-      reponse_iteration = int(input("Veuillez indiquer le nombre d'iteration voulu (entre 0 et 4)"))
-      if reponse_iteration < 0 or reponse_iteration > 4:
-        print("Erreur, mauvais input")
-      else:
-        menu_espace()
-        menu_couleur()
-        reponse_couleur = int(input("Veuillez entrer le nombre correspondant a la couleur choisit"))
-        menu_espace()
-        if reponse_couleur < 1 or reponse_couleur > 3:
-          print("Erreur, mauvais input")
-        elif reponse_couleur == 1:
-          flocon_de_koch(reponse_iteration, "#5766b9")
-        elif reponse_couleur == 2:
-          flocon_de_koch(reponse_iteration, "#b97c57")
-        elif reponse_couleur == 3:
-          flocon_de_koch(reponse_iteration, " #57b9a1 ")
-    elif reponse_menu == 3:
-      menu_espace()
-      menu_iteration(3)
-      reponse_iteration = int(input("Veuillez indiquer le nombre d'iteration voulu (entre 0 et 4)"))
-      if reponse_iteration < 0 or reponse_iteration > 4:
-        print("Erreur, mauvais input")
-      else:
-        menu_espace()
-        arbre_2D(reponse_iteration)
-    elif reponse_menu == 4:
-      menu_espace()
-      arbre_2D_recursif(90)
-    elif reponse_menu == 5:
-      menu_espace()
-      menu_iteration(5)
-      reponse_iteration = int(input("Veuillez indiquer le nombre d'iteration voulu (entre 0 et 6)"))
-      if reponse_iteration < 0 or reponse_iteration > 6:
-        print("Erreur, mauvais input")
-      else:
-        menu_espace()
-        arbre_2D_aleatoire(reponse_iteration)
-      
-#menu()
-
-#Pour voir les arbres, vous pouvez appeler les fonctions :
-#flocon_de_koch() avec un nombre entre 0 et 4 en argument
-#courbe_de_koch() avec un nombre entre 0 et 5 en argument
-#arbre_2D() avec un nombre entre 0 et 4 en argument
-#arbre_2D_recursif(90)
-#arbre_2D_aleatoire() avec un nombre entre 0 et 6 en argument
+#On affiche le menu
+menu()
